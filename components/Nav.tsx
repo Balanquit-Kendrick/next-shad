e17bb0b59e5482d41a57669fa9ddf01d3c5f0ab7
unknown <zxckendrick123@gmail.com>
2024-06-25 22:59:49 +0800

    first commit

---
+++ components/Nav.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/ui/toggle-mode";
+
+export function Nav(){
+    return (
+        <div className="py-16 flex flex-col items-end pe-20">
+            <ModeToggle/>
+        </div>
+    );
+}
